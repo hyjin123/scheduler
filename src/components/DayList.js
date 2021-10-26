43a2b8e7931f7c15ddd329eb20056d9670b3cd85
Sean <seanhoyeonjin@gmail.com>
2021-10-26 19:45:04 -0400

    Added a DayList component using storybook

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,9 +1,21 @@
 import React from "react";
 import classNames from "classnames";
-import "components/DayListItem.scss";
+import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  const days = props.days
+  const newArray = days.map((day) => {
+    return <DayListItem key={day.id}
+    name={day.name}
+    spots={day.spots}
+    selected={day.name === props.day}
+    setDay={props.setDay}
+    />
+  });
+
   return (
-    <ul></ul>
+    <ul>
+      {newArray}
+    </ul>
   );
 }

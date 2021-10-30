97d081044387f16259a20e91d778442da4add1e0
Sean <seanhoyeonjin@gmail.com>
2021-10-29 20:41:07 -0400

    used getInterview function to pass the interview information to the Appointment componenet

--- src/components/Application.js
+++ src/components/Application.js
@@ -38,7 +38,7 @@ export default function Application(props) {
       key={appointment.id}
       id={appointment.id}
       time={appointment.time}
-      interview={interview}
+      interview={getInterview(state, state.appointments[appointment.id].interview)}
     />
   });
 
